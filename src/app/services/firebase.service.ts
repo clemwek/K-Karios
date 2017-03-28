d88e19bbec8de64006bc541547a75855a36df0d3
Clement Wekesa <clemwek@gmail.com>
2017-03-28 09:20:38 +0300

    Add ref to user id

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class FirebaseService {
   something: any;
+  uid: any;
   availableStock: any;
+  obj: any;
 
   constructor(
     private af: AngularFire
-  ) { }
+  ) {
+    this.af.auth.subscribe(auth => {
+      // console.log(auth.uid);
+      this.uid = auth.uid;
+    });
+  }
 
   getSomething() {
     this.something = this.af.database.list('/listings') as FirebaseListObservable<any[]>
@@ -17,7 +25,14 @@ export class FirebaseService {
 
   getGenView(spec) {
     this.availableStock = this.af.database.list('/views/'+spec) as FirebaseListObservable<any[]>
+
+    // console.log(this.uid);
     return this.availableStock;
+  }
+
+  getObj(loc) {
+    this.obj = this.af.database.object(loc) as FirebaseObjectObservable<any>;
+    return this.obj
   }
 
 }
