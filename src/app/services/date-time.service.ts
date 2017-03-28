3dece196c2d3e51a3cad0bca4614ce0674b8d2e9
Clement Wekesa <clemwek@gmail.com>
2017-03-28 09:19:49 +0300

    Add date time functions

--- src/app/services/date-time.service.ts
+++ src/app/services/date-time.service.ts
@@ -2,7 +2,25 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DateTimeService {
+  date: any;
+  dateString: string;
 
   constructor() { }
+
+  formatDate() {
+    this.date = new Date();
+
+    this.dateString = (this.date.getMonth() + 1) + "/" + this.date.getDate() + "/" + this.date.getFullYear().toString();
+
+    return this.dateString;
+  }
+
+  formatDateString() {
+    this.date = new Date();
+
+    this.dateString = String((this.date.getMonth() + 1)) + String(this.date.getDate()) + this.date.getFullYear().toString();
+
+    return this.dateString;
+  }
 
 }
