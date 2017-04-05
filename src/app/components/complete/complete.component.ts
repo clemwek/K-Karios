f66b2cdf31fa113d73bababb376b48309969f853
Clement Wekesa <clemwek@gmail.com>
2017-04-05 12:35:13 +0300

    Add complete component

---
+++ src/app/components/complete/complete.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { DateTimeService } from '../../services/date-time.service';
+import { FirebaseService } from '../../services/firebase.service';
+
+@Component({
+  selector: 'app-complete',
+  templateUrl: './complete.component.html',
+  styleUrls: ['./complete.component.css']
+})
+export class CompleteComponent implements OnInit {
+  complete: any;
+
+  constructor(
+    private firebaseService: FirebaseService,
+    private dateService:DateTimeService
+  ) { }
+
+  ngOnInit() {
+    this.firebaseService.getList('/record/complete/'+this.dateService.formatDateString()+'/').subscribe(complete => {
+      console.log(complete);
+      this.complete = complete;
+    });
+  }
+
+}
