d9ef3f5c87d22a9c4c212f3a4a88ab778d769607
Clement Wekesa <clemwek@gmail.com>
2017-04-10 10:50:07 +0300

    Add admin component

---
+++ src/app/components/admin/admin.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { DateTimeService } from '../../services/date-time.service';
+import { FirebaseService } from '../../services/firebase.service';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+  availableStock: any;
+
+  constructor(
+    private firebaseService: FirebaseService,
+    private dateService:DateTimeService,
+    private userService: UserService
+  ) { }
+
+  ngOnInit() {
+    this.firebaseService.getGenView('availableStock').subscribe(availableStock => {
+      this.availableStock = availableStock;
+    } );
+  }
+
+}
