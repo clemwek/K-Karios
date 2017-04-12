d2d8409e4290cf29846a2b95d99bdfe898e310bf
Clement Wekesa <clemwek@gmail.com>
2017-04-12 12:35:02 +0300

    Suspend making entry to the stockStatus

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -12,6 +12,7 @@ export class AddProductComponent implements OnInit {
   prodName: string;
   volume: number;
   costPUnit: number;
+  prodEndtered: any;
 
   constructor(
     private firebaseService: FirebaseService,
@@ -24,18 +25,39 @@ export class AddProductComponent implements OnInit {
   }
 
   submitOrder() {
-    let data = {
-      name: this.prodName,
-      costPUnit: this.costPUnit,
-      volume: this.volume
-    }
+    this.firebaseService.getObj('/views/availableStock/'+this.prodName).subscribe(prodEndtered => {
+      console.log(prodEndtered)
+      this.prodEndtered = prodEndtered
+    });
 
-    // To do check if the data is in the database 
-    // To do add a success message 
+    console.log(this.prodEndtered);
+    // if (!prodEndtered.name) {
+      let data = {
+        name: this.prodName,
+        costPUnit: this.costPUnit,
+        volume: this.volume
+      }
 
-    this.firebaseService.makeEntryPush('record/admin/stockin/'+this.dateService.formatDateString(), data);
-    this.firebaseService.makeEntrySet('views/availableStock/'+this.prodName, data);
-    this.router.navigate(['admin']);
+      let prodStatus = {
+        name: this.prodName,
+        balCost: (this.costPUnit * this.volume),
+        balVolume: this.volume,
+        inCost: (this.costPUnit * this.volume),
+        inVolume: this.volume,
+        outCost: 0,
+        outVolume: 0
+      }
+      // To do check if the data is in the database 
+      // To do add a success message 
+      // console.log('this is a new entry');
+      this.firebaseService.makeEntryPush('record/admin/stockin/'+this.dateService.formatDateString(), data);
+      this.firebaseService.makeEntrySet('views/availableStock/'+this.prodName, data);
+      this.firebaseService.makeEntrySet('views/admin/stockStatus/'+this.dateService.formatDateString()+'/'+this.prodName, prodStatus);
+      this.router.navigate(['admin']);
+    // } else {
+    //   console.log('The entry is already made');
+    //   this.router.navigate(['admin']);
+    // }
   }
 
 }
