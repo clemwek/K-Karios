cfb8d710f00417ddb9359a1e18fb00470745be46
Clement <clement@localhost.localdomain>
2017-04-19 09:15:49 +0300

    Add adding product

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -11,6 +11,11 @@ import { UserService } from '../../services/user.service';
 export class AdminComponent implements OnInit {
   searchFilter: string;
   availableStock: any;
+  addProd: boolean = false;
+  volume: number;
+  costPUnit: number;
+  listProds: any = [];
+  statusList: any = [];
 
   constructor(
     private firebaseService: FirebaseService,
@@ -21,7 +26,51 @@ export class AdminComponent implements OnInit {
   ngOnInit() {
     this.firebaseService.getGenView('availableStock').subscribe(availableStock => {
       this.availableStock = availableStock;
-    } );
+      this.availableStock.forEach(element => {
+        this.listProds.push(element.name.toLowerCase());
+      });
+    });
+
+    // console.log(this.dateService.formatDateString())
+
+    this.firebaseService.getList('views/admin/stockBal/'+this.dateService.formatDateString()).subscribe(stockStatus => {
+      stockStatus.forEach(element => {
+        this.statusList.push(element.name.toLowerCase())
+      });
+    });
+  }
+
+  showForm() {
+    this.addProd = !this.addProd;
+  }
+
+  submitOrder() {
+    let availableEnt = {
+      name: '',
+      costPUnit: 0,
+      volume: 0
+    }
+    if (this.listProds.indexOf(this.searchFilter.toLowerCase().trim()) === -1) {
+      // This is what happens when making the entry for the first time
+        availableEnt.name = this.searchFilter.toLowerCase();
+        availableEnt.costPUnit = this.costPUnit;
+        availableEnt.volume = this.volume;
+
+      let prodStatus = {
+        // To do: make this work
+      }
+
+      
+      // To Do: add making entry to ball 
+    } else {
+      availableEnt.name = this.searchFilter.toLowerCase();
+      availableEnt.costPUnit = this.costPUnit;
+      availableEnt.volume = Number(this.volume) + Number(this.availableStock[this.listProds.indexOf(this.searchFilter.toLowerCase().trim())].volume)
+      console.log()
+    }
+    this.firebaseService.makeEntrySet('views/availableStock/'+this.searchFilter.toLowerCase().trim(), availableEnt);
+    this.addProd = false;
+    this.searchFilter = this.volume = this.costPUnit = null;
   }
 
 }
